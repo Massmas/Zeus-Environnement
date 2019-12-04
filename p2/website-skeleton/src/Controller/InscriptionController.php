3809f0a995c247f1b316c2b26edf8a353445e5d9
Massmas <33931611+Massmas@users.noreply.github.com>
2019-12-04 17:21:06 +0100

    Inscription

--- p2/website-skeleton/src/Controller/InscriptionController.php
+++ p2/website-skeleton/src/Controller/InscriptionController.php
@@ -7,8 +7,16 @@ use App\Repository\ClientRepository;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use Doctrine\Common\Persistence\ObjectManager;
+
 use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 
 
 
@@ -19,17 +27,46 @@ class InscriptionController extends AbstractController
     /**
      * @Route("/inscription", name="inscription")
      */
-    public function create(Request $request, ObjectManager $manager)
-    {
+    public function create(Request $request, EntityManagerInterface $manager)
+    {   
         $client = new Client();
 
         $form = $this->createFormBuilder($client)
-                     ->add('login',
-                     ['placeholder' => "Votre identifiant"])
-                     ->add('mdp')
-                     ->add('mail')
-                     ->add('dateAt')
+                     ->add('login',TextType::class, [
+                         'attr' => [
+                             'placeholder' => "Votre identifiant",
+                             'class' => 'form-control'
+                         ]
+                     ])
+                     ->add('mdp',PasswordType::class, [
+                        'attr' => [
+                            'placeholder' => "Votre mdp",
+                            'class' => 'form-control'
+                        ]
+                    ])
+                     ->add('mail',EmailType::class, [
+                        'attr' => [
+                            'placeholder' => "Votre mail",
+                            'class' => 'form-control'
+                        ]
+                    ])
+                    ->add('Enregistrer', SubmitType::class, [
+                        'attr' => [
+                            'label' => 'Enregistrer'
+                        ]
+                    ])
                      ->getForm(); 
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $client->setDateAt(new \DateTime());
+
+            $manager->persist($client);
+            $manager->flush();
+        }
+         
+        
 
         return $this->render('inscription/index.html.twig',
     ['formClient' => $form->createView()]);
